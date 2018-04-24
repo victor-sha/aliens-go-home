ae3826701b31d5f2508ebed3a41b24255a0e23a4
Viktor Shaposhnikov <kassini_90@mail.ru>
2018-04-24 17:29:53 +0300

    added cannonBase and cannon

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Sky from './Sky';
 import Ground from './Ground';
+import CannonPipe from './CannonPipe';
+import CannonBase from './CannonBase';
 
 const Canvas = () => {
     const style = { border: '1px solid black' };
@@ -14,7 +16,8 @@ const Canvas = () => {
         >
             <Sky/>
             <Ground/>
-            <circle cx={0} cy={0} r={50} />
+            <CannonPipe rotation={45}/>
+            <CannonBase />
         </svg>
     );
 };
