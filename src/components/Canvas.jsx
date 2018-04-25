f255db8197296360cce1d8f061adfaeaf29356de
Viktor Shaposhnikov <kassini_90@mail.ru>
2018-04-25 11:34:17 +0300

    added action creator moveObjects

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Sky from './Sky';
 import Ground from './Ground';
 import CannonPipe from './CannonPipe';
 import CannonBase from './CannonBase';
 
-const Canvas = () => {
-    const style = { border: '1px solid black' };
+const Canvas = (props) => {
     const viewBox =  [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+
     return (
         <svg
          id='aliens-go-home-canvas'
          preserveAspectRatio='zMaxYMax none'
+         onMouseMove={props.trackMouse}
          viewBox={viewBox}
-         style={style}
         >
             <Sky/>
             <Ground/>
-            <CannonPipe rotation={45}/>
+            <CannonPipe rotation={props.angle}/>
             <CannonBase />
         </svg>
     );
 };
+
+Canvas.propTypes = {
+    angle: PropTypes.number.isRequired,
+    trackMouse: PropTypes.func.isRequired,
+  };
 
 export default Canvas;
